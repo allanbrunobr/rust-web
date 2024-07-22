f7c40b3bd8ec5d0cf5f019cacf9340a18e2689ff
allanbrunobr <allanbruno@gmail.com>
2024-07-22 01:11:50 -0300

    Remove target directory from version control

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn index () -> &'static str{
+    "Hello Rocket"
+}
+
+#[get("/hi")]
+fn hi () -> &'static str{
+    "Hi Rocket"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/v1", routes![index, hi])
+}
